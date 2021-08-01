b0f05fdc4a6062f9696979d9abc628a4c012b3af
Abhilash <abhilash.asokan@logicline.de>
2021-08-01 09:02:40 +0530

    Initial Commit

---
+++ My Instagram/My_InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_InstagramApp.swift
+//  My Instagram
+//
+//  Created by Abhilash Asokan on 01/08/21.
+//
+
+import SwiftUI
+
+@main
+struct My_InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
